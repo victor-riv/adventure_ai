e9f3ba65fc47488cb1656e8e91478b438d30aa1a
Victor Rivera <verm2405@gmail.com>
2024-01-05 18:08:38 -0500

    Init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:adventure_ai/screens/landing_page_screen.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:adventure_ai/screens/landing_page_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const ProviderScope(child: MyApp()));
 }
 
